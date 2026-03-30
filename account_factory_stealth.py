e073158cf207601f8716e4f27450e73b056d3a06
shootmusic <shootmusic05@gmail.com>
2026-03-31 00:27:40 +0700

    fix: escape single quote di baris 253

--- account_factory_stealth.py
+++ account_factory_stealth.py
@@ -250,7 +250,7 @@ class StealthAccountFactory:
                 "//div[contains(text(), 'Choose your email')]",
                 "//div[contains(text(), 'username')]",
                 "//div[contains(text(), 'Gmail address')]",
-                "//div[contains(text(), 'You'll use this to sign in')]"
+                "//div[contains(text(), "You'll use this to sign in")]"
             ]
             
             is_username_page = False
